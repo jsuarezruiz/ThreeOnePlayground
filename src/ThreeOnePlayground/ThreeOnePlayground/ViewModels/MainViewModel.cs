322756766d3eb2a6f373e6c85eacaa5909c7081a
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-07-22 13:32:37 +0200

    Initial Commit

---
+++ src/ThreeOnePlayground/ThreeOnePlayground/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Input;
+using ThreeOnePlayground.ViewModels.Base;
+using Xamarin.Forms;
+
+namespace ThreeOnePlayground.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ICommand AutoResizeEditorCommand => new Command(AutoResizeEditor);
+        public ICommand EntryAutoCapitalizationCommand => new Command(EntryAutoCapitalization);
+        public ICommand EntryReturnKeyCommand => new Command(EntryReturnKey);
+        public ICommand TabsCommand => new Command(Tabs);
+        public ICommand OthersCommand => new Command(Others);
+
+        private void AutoResizeEditor()
+        {
+            NavigationService.NavigateToAsync<AutoResizeEditorViewModel>();
+        }
+
+        private void EntryAutoCapitalization()
+        {
+            NavigationService.NavigateToAsync<EntryAutoCapitalizationViewModel>();
+        }
+
+        private void EntryReturnKey()
+        {
+            NavigationService.NavigateToAsync<EntryReturnKeyViewModel>();
+        }
+
+        private void Tabs()
+        {
+            NavigationService.NavigateToAsync<TabsViewModel>();
+        }
+
+        private void Others()
+        {
+            NavigationService.NavigateToAsync<OthersViewModel>();
+        }
+    }
+}
